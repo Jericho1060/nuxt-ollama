bc3d5ac5c9227141bfeec0dde7387ddbb18d1f3a
Thomas <jericho1060@gmail.com>
2024-08-06 17:26:14 +0200

    fix browser side

--- src/runtime/composables/useOllama.ts
+++ src/runtime/composables/useOllama.ts
@@ -1,1 +1,11 @@
-export { useOllama } from '../useOllama'
+import { Ollama } from 'ollama/browser'
+import type { ModuleOptions } from '../../module'
+import { useRuntimeConfig } from '#imports'
+
+export function useOllama() {
+  const options: ModuleOptions = useRuntimeConfig().public.ollama as ModuleOptions
+  return new Ollama({
+    host: `${options.protocol}://${options.host}:${options.port}`,
+    proxy: options.proxy,
+  })
+}
