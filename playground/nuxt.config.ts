c8169b9790f5f0f635c4fa8cf058119a0021527a
Thomas <github.passage306@passmail.net>
2025-04-15 20:29:08 +0200

    upgrade

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,9 +1,12 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   devtools: { enabled: true },
+
   ollama: {
     protocol: 'http',
     host: 'localhost',
     port: 11434,
   },
+
+  compatibilityDate: '2025-04-15',
 })
